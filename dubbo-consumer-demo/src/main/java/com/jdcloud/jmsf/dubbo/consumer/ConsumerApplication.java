e805453b47f24a3374c11ae53b774514a879287a
chenzhiguo5 <chenzhiguo@jd.com>
2024-09-05 16:08:59 +0800

    Clean

--- dubbo-consumer-demo/src/main/java/com/jdcloud/jmsf/dubbo/consumer/ConsumerApplication.java
+++ dubbo-consumer-demo/src/main/java/com/jdcloud/jmsf/dubbo/consumer/ConsumerApplication.java
@@ -16,19 +16,6 @@ public class ConsumerApplication {
      * launch the application
      */
     public static void main(String[] args) {
-        System.setProperty("JMESH_SERVICE_ID", "CustomConsumerService");
-        // System.setProperty("JMESH_REGISTRY_TOKEN", "6fca9ebf-ab1d-8023-9348-e6e12649968f");
-        System.setProperty("JMESH_SERVICE_NAMESPACE", "default");
-        System.setProperty("JMESH_SERVICE_APP", "jmsf-consumer-demo");
-        System.setProperty("JMESH_SERVICE_VERSION", "v1.0");
-        System.setProperty("JMESH_SERVICE_GROUP", "group1");
-        System.setProperty("JMESH_SERVICE_CLUSTER", "cluster-dev");
-        System.setProperty("JMESH_REGISTRY_HOST", "127.0.0.1");
-        System.setProperty("JMESH_REGISTRY_PORT", "8500");
-        System.setProperty("JMESH_MESH_GROUP", "mesh01");
-        System.setProperty("JMESH_SERVICE_DEPLOYMENT", "dep1");
-        System.setProperty("ML_AZ", "zone1");
-        // System.setProperty("JMESH_ENABLE_MESH", "true");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
         context.start();
         DemoService service = null;
--- dubbo-provider-demo/src/main/java/com/jdcloud/jmsf/dubbo/provider/ProviderApplication.java
+++ dubbo-provider-demo/src/main/java/com/jdcloud/jmsf/dubbo/provider/ProviderApplication.java
@@ -26,18 +26,6 @@ import org.springframework.context.annotation.PropertySource;
 public class ProviderApplication {
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("JMESH_SERVICE_ID", "CustomProviderService");
-        // System.setProperty("JMESH_REGISTRY_TOKEN", "6fca9ebf-ab1d-8023-9348-e6e12649968f");
-        System.setProperty("JMESH_SERVICE_NAMESPACE", "default");
-        System.setProperty("JMESH_SERVICE_APP", "jmsf-provider-demo");
-        System.setProperty("JMESH_SERVICE_VERSION", "v1.0");
-        System.setProperty("JMESH_SERVICE_GROUP", "group1");
-        System.setProperty("JMESH_SERVICE_CLUSTER", "cluster-dev");
-        System.setProperty("JMESH_REGISTRY_HOST", "127.0.0.1");
-        System.setProperty("JMESH_REGISTRY_PORT", "8500");
-        System.setProperty("JMESH_MESH_GROUP", "mesh01");
-        System.setProperty("JMESH_SERVICE_DEPLOYMENT", "dep1");
-        System.setProperty("ML_AZ", "zone1");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProviderConfiguration.class);
         context.start();
         System.in.read();
